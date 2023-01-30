7aefc3d145833b324bea72ca3bbad8212e4325e9
samm-o <samuilsgeorgiev@gmail.com>
2023-01-30 09:57:55 -0500

    Add project files.

---
+++ Longest word in File/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Longest_word_in_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "(insert file pathway here)";
+            var words = File.ReadAllText(path).Split(" ");
+            string longest = words.OrderByDescending(s => s.Length).First();
+            Console.WriteLine(longest);
+        }
+    }
+}
